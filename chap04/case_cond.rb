c4c9dee8e464788fdbeee886a2806ef9464981e0
Shusuke Nakamura <shusuke.lifebook@gmail.com>
2025-10-27 20:56:56 +0900

    4.1条件式の学習完了

---
+++ chap04/case_cond.rb
@@ -0,0 +1,7 @@
+point = 65
+
+if point >= 60
+  puts '合格'
+elsif point < 60
+  puts '不合格'
+end
---
+++ chap04/pattern_assign.rb
@@ -0,0 +1,4 @@
+case { title: '独習Java', publisher: '翔泳社', authors: %w[山田太郎 山本次郎] }
+in { title: _, publisher: '翔泳社', authors: ['山田太郎', *] => authors }
+  puts authors
+end
